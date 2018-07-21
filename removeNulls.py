861fba75b22540e9b7e6558c53247cf955339e63
Elton Matimbe <eltonmatimbe@Eltons-MBP.lan>
2018-07-21 18:35:40 +1000

    test jenkins

--- removeNulls.py
+++ removeNulls.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Load data
+# Load data from csv
 melb_data = pd.read_csv('data/melb_data.csv')
 
 from sklearn.ensemble import RandomForestRegressor
@@ -8,6 +8,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 melb_target = melb_data.Price
+
 melb_predictors = melb_data.drop(['Price'], axis=1)
 
 # For the sake of keeping the example simple, we'll use only numeric predictors. 
